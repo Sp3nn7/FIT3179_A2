371281097ab1746b99de9809a8aefbf3eabd50a7
Spenc <sger0007@student.monash.edu>
2023-10-05 09:38:16 +1100

    adding some basic css formatting, embedding vega lite, creating vg file

---
+++ Idioms/Waffle_Chart/waffle_chart.js
@@ -0,0 +1,4 @@
+var vg_1 = "waffle_chart.vg.json";
+vegaEmbed("#waffle_chart_idiom", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

346255271479d22f8640c5455ab4c778721a2fd8
Spenc <sger0007@student.monash.edu>
2023-10-05 12:58:52 +1100

    adding additional folder for our tree map idiom

---
+++ Idioms/Tree_Map/tree_map.js
@@ -0,0 +1,4 @@
+var vg_1 = "tree_map.vg.json";
+vegaEmbed("#tree_map_idiom", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

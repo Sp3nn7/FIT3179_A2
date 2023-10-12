5fdf9dd54619e79ce0a8da4fabe205e16f6e1e67
Spenc <sger0007@student.monash.edu>
2023-10-12 12:25:08 +1100

    adding a box and whisker plot idiom

---
+++ Idioms/Box_And_Whisker_Plot/box_and_whisker_plot.js
@@ -0,0 +1,4 @@
+var vg_1 = "donut_chart.vg.json";
+vegaEmbed("#donut_chart_idiom", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

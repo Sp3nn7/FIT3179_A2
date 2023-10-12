e1b0c017718a8a87a701a58923a1ca4539269844
Spenc <sger0007@student.monash.edu>
2023-10-12 13:18:47 +1100

    making some naming amendments

--- Idioms/Map_Idiom/map_idiom.js
+++ Idioms/Map_Idiom/map_idiom.js
@@ -1,4 +1,4 @@
-var vg_1 = "W9_homework.vg.json";
+var vg_1 = "map_idiom.vg.json";
 vegaEmbed("#map_idiom", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);

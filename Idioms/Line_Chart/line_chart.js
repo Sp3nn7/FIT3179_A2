b429694927094003020a0a9729b87f2173295763
Spenc <sger0007@student.monash.edu>
2023-10-13 16:47:56 +1100

    creating a css formatting and adding additional idioms

---
+++ Idioms/Line_Chart/line_chart.js
@@ -0,0 +1,4 @@
+var vg_1 = "line_chart.vg.json";
+vegaEmbed("#line_chart_idiom", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
